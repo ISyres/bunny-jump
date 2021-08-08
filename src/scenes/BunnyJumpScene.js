cb7b1427f00ba5c83a753b05c5fc47c661db0e35
Erpriliano Abbas <erpriliano@gmail.com>
2021-08-08 19:16:52 +0700

    Make player and platforms collide

--- src/scenes/BunnyJumpScene.js
+++ src/scenes/BunnyJumpScene.js
@@ -1,6 +1,7 @@
 import Phaser from 'phaser'
 
-var platforms;
+var platforms
+var player
 
 export default class BunnyJumpScene extends Phaser.Scene {
     constructor() {
@@ -12,24 +13,29 @@ export default class BunnyJumpScene extends Phaser.Scene {
         this.load.image('platform', 'images/ground_grass.png')
         this.load.image('carrot', 'images/carrot.png')
         this.load.image('bunny_jump', 'images/bunny1_jump.png')
+        this.load.image('bunny_stand', 'images/bunny1_stand.png')
     }
 
     create() {
         this.add.image(240, 320, 'background')
 
         // Duplicate platforms
-        this.platforms = this.physics.add.staticGroup()
+        platforms = this.physics.add.staticGroup()
 
         for (let i = 0; i < 5; i++) {
             const x = Phaser.Math.Between(80, 400)
             const y = 150 * i
-            const platformChild = this.platforms.create(x, y, 'platform')
+            const platformChild = platforms.create(x, y, 'platform')
             platformChild.setScale(0.5)
             platformChild.refreshBody()
 
             const body = platformChild.body
             body.updateFromGameObject()
         }
+
+        // Create player
+        player = this.physics.add.sprite(240, 320, 'bunny_stand').setScale(0.5)
+        this.physics.add.collider(player, platforms)
     }
 
     update() {
