a057922664c7979a101d1d4738a8c6f2d0358235
Erpriliano Abbas <erpriliano@gmail.com>
2021-08-08 18:59:51 +0700

    Initial configuration

---
+++ src/scenes/BunnyJumpScene.js
@@ -0,0 +1,19 @@
+import Phaser from 'phaser'
+
+export default class BunnyJumpScene extends Phaser.Scene {
+    constructor() {
+        super('bunny-jump-scene')
+    }
+
+    preload() {
+        
+    }
+
+    create() {
+
+    }
+
+    update() {
+        
+    }
+}
